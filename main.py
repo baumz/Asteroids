4ecede2641e298415c50cb2aad2acad755d3fb2e
github_username_here <5067564+baumz@users.noreply.github.com>
2025-10-14 14:34:30 -0500

    Set up framerate

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ from constants import *
 
 def main():
     pygame.init()
+    
+    dt = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    game_clock = pygame.time.Clock()
 
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -15,7 +19,7 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
-
+        dt = game_clock.tick(60)
 
 
 if __name__ == "__main__":
