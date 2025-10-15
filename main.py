292efb598c039f5f4ccf11c5f3dc57517b9e0dfe
github_username_here <5067564+baumz@users.noreply.github.com>
2025-10-15 13:33:50 -0500

    Grouped drawing and updateing. also fixed issues with player speed.

--- main.py
+++ main.py
@@ -4,12 +4,18 @@ from player import *
 
 def main():
     pygame.init()
-    player_1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-    
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     dt = 0
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     game_clock = pygame.time.Clock()
+
+    player_1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
 
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -20,12 +26,14 @@ def main():
             if event.type == pygame.QUIT:
                 return
         
-        player_1.update(dt)
+        dt = game_clock.tick(60)
+
         #Draw
         screen.fill("black")
-        player_1.draw(screen)
+        updatable.update(dt/1000)
+        for p in drawable:
+            p.draw(screen)
         pygame.display.flip()
-        dt = game_clock.tick(60)
 
 
 if __name__ == "__main__":
